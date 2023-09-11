5f6f25b0468b2ba93bd7f03c5b07daf3dafa4eee
Harshitrahadve <yashrahadve@gmail.com>
2023-09-11 10:55:43 +0530

    01 counter is completed

---
+++ 01 Counter/script.js
@@ -0,0 +1,21 @@
+const incriment = document.querySelector("#prevCount");
+const decriment = document.querySelector("#nextCount");
+const value = document.querySelector("#output");
+const reset = document.querySelector(".reset");
+
+
+incriment.addEventListener('click', () => {
+  value.innerHTML =+ value.textContent - changeBy.valueAsNumber;
+})
+
+
+decriment.addEventListener('click', () => {
+  value.innerHTML =+ value.textContent + changeBy.valueAsNumber;
+})
+
+reset.addEventListener("click", () => {
+  value.innerText = 0;
+})
+
+
+
